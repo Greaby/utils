d0057490f4be0fe238e65b4fafb6856acd048a7d
Greaby <contact@greaby.com>
2018-05-25 18:37:22 +0200

    create a batch to launch all apps I need to stream

---
+++ setup_stream_app.bat
@@ -0,0 +1,14 @@
+SET directory=%~dp0
+
+nircmd setdefaultsounddevice "Casque" 1
+
+start "smg" /D "D:\Documents\greaby\live\SMG" "smg.exe"
+start "vlc" /D "C:\Program Files (x86)\VideoLAN\VLC" "vlc.exe" --loop --random "%directory%audio" --directx-volume=0.35
+start "obs" /D "C:\Program Files\OBS" "OBS.exe"
+start "character-animator" /D "C:\Program Files\Adobe\Adobe Character Animator CC (Beta)\Support Files" "Character Animator.exe"
+
+start "restream" /D "C:\Users\jeremy\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Restream.io" "Restream Chat.appref-ms"
+
+"C:\Program Files\Mozilla Firefox\firefox.exe" -url "https://tweetdeck.twitter.com/" "https://www.youtube.com/live_dashboard" "https://www.twitch.tv/greaby/dashboard/live"
+
+exit
